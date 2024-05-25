74c52dbe76e158bce46752806dcf7a539badb44a
Akshath Sai Pittala <akshathsai.pittala@outlook.com>
2024-05-26 00:26:06 +0530

    - Code refactoring

--- src/main/java/com/akshathsaipittala/streamspace/web/controllers/StreamController.java
+++ src/main/java/com/akshathsaipittala/streamspace/web/controllers/StreamController.java
@@ -3,7 +3,6 @@ package com.akshathsaipittala.streamspace.web.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -16,10 +15,9 @@ import java.nio.charset.StandardCharsets;
 @RequestMapping("/stream")
 public class StreamController {
 
-    @GetMapping("/movie/{contentId}")
-    public String getVideoPlayer(@PathVariable("contentId") String contentId, Model model) {
-        log.info("pathd {}", StringUtils.cleanPath(contentId));
-        model.addAttribute("contentId", StringUtils.cleanPath(contentId));
+    @GetMapping("/movie/{movieCode}")
+    public String getVideoPlayer(@PathVariable("movieCode") String movieCode, Model model) {
+        model.addAttribute("movieCode", movieCode);
         return "player :: videoPlayer";
     }
 

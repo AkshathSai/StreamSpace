a5f36b117313b6ebecd516a4f9b43b0f8d9fd201
Akshath Sai Pittala <akshathsai.pittala@outlook.com>
2024-05-25 23:44:11 +0530

    - PWA not suitable - reverting

--- src/main/java/com/akshathsaipittala/streamspace/web/controllers/WebController.java
+++ src/main/java/com/akshathsaipittala/streamspace/web/controllers/WebController.java
@@ -1,6 +1,5 @@
 package com.akshathsaipittala.streamspace.web.controllers;
 
-import org.progressify.spring.annotations.StaleWhileRevalidate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -8,7 +7,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 public class WebController {
 
     @GetMapping("/")
-    @StaleWhileRevalidate
     public String index() {
         return "index";
     }

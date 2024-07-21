0a75d1d25af1671bed8858cca9487cd91f55900e
Akshath Sai Pittala <akshathsai.pittala@outlook.com>
2024-07-21 19:45:35 +0530

    - Major Overhaul in progress

--- src/main/java/com/akshathsaipittala/streamspace/repository/MovieContentStore.java
+++ src/main/java/com/akshathsaipittala/streamspace/repository/MovieContentStore.java
@@ -3,9 +3,7 @@ package com.akshathsaipittala.streamspace.repository;
 import com.akshathsaipittala.streamspace.library.Movie;
 import org.springframework.content.commons.store.ContentStore;
 import org.springframework.content.rest.StoreRestResource;
-import org.springframework.stereotype.Component;
 
-@Component
 @StoreRestResource
 public interface MovieContentStore extends ContentStore<Movie, String> {
 
--- src/main/java/com/akshathsaipittala/streamspace/repository/MusicStore.java
+++ src/main/java/com/akshathsaipittala/streamspace/repository/MusicStore.java
@@ -3,8 +3,6 @@ package com.akshathsaipittala.streamspace.repository;
 import com.akshathsaipittala.streamspace.library.Song;
 import org.springframework.content.commons.store.ContentStore;
 import org.springframework.content.rest.StoreRestResource;
-import org.springframework.stereotype.Component;
 
-@Component
 @StoreRestResource(path="music")
 public interface MusicStore extends ContentStore<Song, String> {}

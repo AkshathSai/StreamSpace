f4b06fab22a2fc9256fe17240c28df9767837d34
Akshath Sai Pittala <akshathsai.pittala@outlook.com>
2024-07-29 23:10:49 +0530

    - Major Overhaul in progress

--- src/main/java/com/akshathsaipittala/streamspace/www/StreamController.java
+++ src/main/java/com/akshathsaipittala/streamspace/www/StreamController.java
@@ -1,6 +1,9 @@
 package com.akshathsaipittala.streamspace.www;
 
+import com.akshathsaipittala.streamspace.library.VideoRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,11 +16,25 @@ import java.nio.charset.StandardCharsets;
 @Slf4j
 @Controller
 @RequestMapping("/stream")
+@RequiredArgsConstructor
 public class StreamController {
+
+    final VideoRepository videoRepository;
 
     @GetMapping("/video/{movieCode}")
     public String getVideoPlayer(@PathVariable("movieCode") String movieCode, Model model) {
         model.addAttribute("movieCode", movieCode);
+        var video = videoRepository.findById(URLDecoder.decode(movieCode, StandardCharsets.UTF_8));
+        var contentMimeType = MediaType.APPLICATION_OCTET_STREAM_VALUE;
+
+        // Workaround to get MKV Videos playing on Chromium browsers
+        if (video.get().getContentMimeType().equals("video/x-matroska")) {
+            contentMimeType = "video/webm";
+        } else if (video.get().getContentMimeType().equals("video/mp4")) {
+            contentMimeType = "video/mp4";
+        }
+
+        model.addAttribute("contentMimeType", contentMimeType);
         return "player :: videoPlayer";
     }
 
